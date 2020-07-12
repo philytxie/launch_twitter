b7278089187ef249316c6edcb4d54e591299b3dd
phil <philytxie@gmail.com>
2020-07-12 17:50:27 -0400

    profile form is functional and we link properly from new_profile page; created show profile screen; only show my profile link if my profile exists

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,33 @@
 class ProfilesController < ApplicationController
+	before_action :authenticate_user!
+
+	def new
+		@profile = Profile.new
+	end
+
+	def create
+		@profile = Profile.new(profile_params)
+		@profile.user = current_user
+		if @profile.save
+			flash[:success] = "Your profile was successfully made"
+			redirect_to new_tweet_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@profile = Profile.find(params[:id])
+	end
+	
+	def profile_params
+		params.require(:profile).permit(:first_name, :last_name, :age)
+	end
+
+
+
+
+
+
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,18 @@
 class StaticPagesController < ApplicationController
 	def main_page
-		@mission_statement = "This website was created on 7/11/2020 by Phil Xie during the 'Code Twitter in a Weekend' coding bootcamp provided by LaunchX. Welcome and feel free to click around!" 
+		if user_signed_in?
+			if current_user.profile
+				redirect_to new_tweet_path
+			else
+				redirect_to new_profile_path
+			end
+		else
+			@mission_statement = "This website was created on 7/11/2020 by Phil Xie during the 'Code Twitter in a Weekend' coding bootcamp provided by LaunchX. Welcome and feel free to click around!" 
 		# `@` transfers the code to the main page
 		# <% %> tells that it is ruby code
+		end
 	end
+	
 
 
 end
