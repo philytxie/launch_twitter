8de00c9ffaf5899ea53603496424fe6d6c4cc02e
phil <philytxie@gmail.com>
2020-07-12 14:52:16 -0400

    connect users to tweet in tweets#create and authenticate users in tweets controller. Only show new tweet button if user is signed in

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,19 @@
 class TweetsController < ApplicationController
+	
+	before_action :authenticate_user!
+
 	def new 
 		@tweet = Tweet.new
 	end
 
 	def create
 		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
 		if @tweet.save
 			flash[:success] = "Woohoo! Your tweet is successfully published."
 			redirect_to new_tweet_path
 		else 
+			p @tweet.errors.inspect
 			render 'new'
 			# once we're in here, there's a .errors
 		end
