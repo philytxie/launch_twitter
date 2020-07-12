fd2d36ae92a46740530697a7eb26f099bd344e73
phil <philytxie@gmail.com>
2020-07-11 20:48:57 -0400

    setup log out function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 	def main_page
-		@mission_statement = "save the world" 
+		@mission_statement = "This website was created on 7/11/2020 by Phil Xie during the 'Code Twitter in a Weekend' coding bootcamp provided by LaunchX. Welcome and feel free to click around!" 
 		# `@` transfers the code to the main page
 		# <% %> tells that it is ruby code
 	end
